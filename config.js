e8f9af47b8591087c0a85b8238ffc14978110f5d
2genrips <willingpaige@gmail.com>
2026-09-04 00:54:08 -0400

    Update config.js

--- config.js
+++ config.js
@@ -41,7 +41,7 @@
   }
 */
 window.TWOGEN_CONFIG = {
-  inventoryApiBase: "",
+  inventoryApiBase: "https://2gen-vault-inventory.willingpaige.workers.dev"
   supabaseUrl: "",
   supabaseAnonKey: "",
   appVersion: "5.0.0"
