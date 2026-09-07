a63fb5f9406d0165879f6da96b1787ea3b0058f4
2genrips <willingpaige@gmail.com>
2026-09-07 09:55:54 -0400

    Bump VaultSignal to v24 Scout Proof

--- config.js
+++ config.js
@@ -23,12 +23,12 @@ window.TWOGEN_CONFIG = {
   premiumMonthlyPrice: 4.99,
   premiumProductId: "vaultsignal_premium_monthly",
   premiumPreview: true,
-  releaseChannel: "hunt-missions",
+  releaseChannel: "scout-proof",
   premiumEntitlementApiBase: "",
   supportEmail: "",
   supportUrl: "",
   privacyUrl: "./privacy.html",
   termsUrl: "./terms.html",
   dataDeletionUrl: "./privacy.html#deletion",
-  appVersion: "23.0.0"
+  appVersion: "24.0.0"
 };
